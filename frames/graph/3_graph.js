d40562bcf54e9a9eef729dd581c743b0dc3a1035
ppicoult <pierre.picoult@gmail.com>
2021-09-02 14:44:20 +0200

    update

---
+++ frames/graph/3_graph.js
@@ -0,0 +1,138 @@
+var pivot3 = new WebDataRocks({
+    container: "#pivot3",
+    toolbar: true, //lepivs boutons du bandeau de base sont masqués et remplacés par le bouton custom d'appel d'un csv local
+    height: 400,
+    width: 400,
+    report: {
+			"dataSource": {
+          "dataSourceType": "csv",
+					"filename": "https://zw6072.github.io/depot_data/MISYSPO.csv" //seules les sources en ligne peuvent être lues, sauf à passer par le bouton d'appel d'un csv
+      },
+      "options": {
+        "grid": {
+            "showGrandTotals": "on"
+        },
+      },
+      "formats": [
+        {
+        "name": "",
+        "decimalPlaces": 0
+        },
+      ],
+			"slice": {
+				"reportFilters": [
+            {
+                "uniqueName": "sid",
+                "filter": {
+                    "members": [
+                        "sid.0"  //filtre le rapport avec "sid" = "0"
+                    ]
+                }
+            },
+            {
+            "uniqueName": "ent", //champs "ent" en ligne
+            "filter": {
+                "members": [
+                    "ent.0"  //filtre le rapport avec "sid" = "0"
+                ]
+              }
+            },
+        ],
+				"columns": [
+				],
+				"rows": [
+          {
+          "uniqueName": "npollutant_K",
+          },
+				],
+				"measures": [
+          {
+          "uniqueName": "vpollutant",
+          "aggregation": "sum",
+          },
+	 			],
+			},
+		},
+    reportcomplete: function() {
+      pivot3.off("reportcomplete");
+      createChart3(); //quand le pivot est créé, le chart peut être créé
+    }
+});
+
+function createChart3() { //le chart est créé
+        pivot3.getData({}, drawChart3, updateChart3);
+    };
+
+function prepareDataFunction10(rawData) {
+    var result = {};
+    var labels = [];
+    var data = [];
+    for (var i = 0; i < rawData.data.length; i++) {
+        var record = rawData.data[i];
+        if (record.c0 == undefined && record.r0 !== undefined) {
+            var _record = record.r0;
+            labels.push(_record);
+        }
+        if (record.c0 == undefined & record.r0 == undefined) continue;
+        if (record.v0 != undefined) {
+            data.push(!isNaN(record.v0) ? record.v0 : null);
+        }
+    }
+    result.labels = labels;
+    result.data = data;
+    return result;
+};
+
+function drawChart3(rawData) {
+    var data = prepareDataFunction10(rawData);
+    var data_for_charts = {
+        datasets: [{
+            data: data.data,
+            borderWidth: 2,
+            backgroundColor: [
+              'rgb(255, 99, 132, 0.7)',
+              'rgba(74, 100, 195, 0.7)',
+              'rgba(241, 196, 15, 0.7)',
+              'rgba(14, 102, 85, 0.7)',
+            ],
+            tension: 0.5,
+            stepped: true,
+            fill: false,
+            cutout: "60%",
+            //stack: ,
+        },
+      ],
+        labels: ['CO2 (kg)','NOX (kg)', 'VOC (kg)','PM (kg)'],
+    };
+    options = {
+        plugins: {
+          responsive: true,
+          legend: {
+              display: true,
+              position: 'right',
+          },
+          title: {
+              display: false,
+              fontSize: 18,
+              text: 'Profit by Countries'
+          },
+        },
+        elements: {
+          point: {
+            pointStyle: 'dash',
+          },
+        },
+    };
+
+    var ctx = document.getElementById("chartcontainer3").getContext('2d');
+		var chart = new Chart(ctx, {
+        data: data_for_charts,
+        type: 'doughnut',
+        options: options
+    });
+};
+
+function updateChart3(rawData) {
+    chart.destroy10();
+    drawChart3(rawData);
+};
