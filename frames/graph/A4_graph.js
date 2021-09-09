b08568e5d51c57c5c498b11a97baa2beb6fac767
ppicoult <pierre.picoult@gmail.com>
2021-09-09 16:17:38 +0200

    update

---
+++ frames/graph/A4_graph.js
@@ -0,0 +1,36 @@
+var ctx = document.getElementById("chartcontainerA4");
+var chartcontainerA4 = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['High QoS', 'Minimum QoS'],
+    datasets: [{
+      label: '',
+      data: [51.71, 87.54],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(60, 198, 205, 0.5)',
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(60, 198, 205, 1)',
+      ],
+      borderWidth: 0,
+      barThickness: 80,
+    }]
+  },
+  options: {
+    indexAxis: 'y',
+    responsive: false,
+    scales: {
+      y: {
+          max: 100,
+          min: 0,
+        },
+      },
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  }
+});
